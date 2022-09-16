424439b9d47c1d3937f8398238f73fb62e12fd4b
Dan Selman <danscode@selman.org>
2022-09-16 12:45:32 +0100

    wip
    Signed-off-by: Dan Selman <danscode@selman.org>

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ const jsonata = require("jsonata");
 
 const transform = require('@accordproject/markdown-transform').transform;
 
-const JSONATA = `(
-    $replaceVariable := | **.nodes[\`$class\`='org.accordproject.templatemark.VariableDefinition'] | {"$class": "org.accordproject.commonmark.Text", "text" : "my variable value"}, ['identifiedBy', 'name', 'elementType']|;
-    $ ~> $replaceVariable
-)`;
+function getTransform(variableName, textValue) {
+    return `(
+        $replaceVariable := | **.nodes[\`$class\`='org.accordproject.templatemark.VariableDefinition' and name='${variableName}'] | {"$class": "org.accordproject.commonmark.Text", "text" : \"${textValue}\"}, ['identifiedBy', 'name', 'elementType']|;
+        $ ~> $replaceVariable
+    )`;
+}
+
+const QUERY_VARIABLES = `**.nodes[\`$class\`='org.accordproject.templatemark.VariableDefinition']`;
 
 /**
  * Prepare the text for parsing (normalizes new lines, etc)
  * @param {string} input - the text for the clause
  * @return {string} - the normalized text for the clause
  */
- function normalizeNLs(input) {
+function normalizeNLs(input) {
     // we replace all \r and \n with \n
-    let text =  input.replace(/\r/gm,'');
+    let text = input.replace(/\r/gm, '');
     return text;
 }
 
@@ -30,7 +34,7 @@ function loadModels(dir) {
 async function run() {
     const acceptanceGrammarFile = path.resolve(__dirname, 'data/acceptance', 'grammar.tem.md');
     const acceptanceGrammar = normalizeNLs(fs.readFileSync(acceptanceGrammarFile, 'utf8'));
-    const acceptanceModelDir =  path.resolve(__dirname, 'data/acceptance');
+    const acceptanceModelDir = path.resolve(__dirname, 'data/acceptance');
 
     const models = loadModels(acceptanceModelDir);
     const parameters = { inputFileName: acceptanceGrammar, template: acceptanceGrammar, model: models, templateKind: 'contract' };
@@ -38,14 +42,34 @@ async function run() {
     const options = {
         verbose: false
     }
-    const json = await transform(acceptanceGrammar, 'markdown_template', ['templatemark'], parameters, options);
-    console.log(JSON.stringify(json, null,2));
+    let json = await transform(acceptanceGrammar, 'markdown_template', ['templatemark'], parameters, options);
+    // console.log(JSON.stringify(json, null,2));
 
-    const expression = jsonata(JSONATA);
-    const result = expression.evaluate(json);
-    console.log(JSON.stringify(result, null,2));
+    const queryVariables = jsonata(QUERY_VARIABLES);
+    const variables = queryVariables.evaluate(json);
 
-    const md = await transform(result, 'templatemark', ['markdown_template'], parameters, options);
+    const values = {
+        shipper: 'Acme Shipping',
+        receiver: 'Dan Warehouses',
+        deliverable: 'Widgets',
+        receiver: 'Dan Selman',
+        businessDays: '10 days',
+        attachment: 'Appendix A'
+    }
+
+    variables.forEach(variable => {
+        console.log(variable.name);
+        const variableValue = values[variable.name];
+        if(variableValue) {
+            const mergeVariables = jsonata(getTransform(variable.name, variableValue));
+            json = mergeVariables.evaluate(json);
+            delete values[variable.name];
+        }
+    })
+
+    // console.log(JSON.stringify(result, null,2));
+
+    const md = await transform(json, 'templatemark', ['markdown_template'], parameters, options);
     console.log(md);
 }
 
